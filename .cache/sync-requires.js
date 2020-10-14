90ef0d7f2e53b37ecd946743388deb6d89d5812f
Rob Wills <robwills@Robs-MacBook-Pro.local>
2020-10-14 20:08:29 +0100

    Basic layout

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/robwills/Sites/GATSBY/Gatsby-Boilerplate/.cache/dev-404-page.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/robwills/Sites/GATSBY/Gatsby-Boilerplate/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/robwills/Sites/GATSBY/Gatsby-Boilerplate/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/robwills/Sites/GATSBY/Gatsby-Boilerplate/src/pages/index.js")))
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function HomePage() {
+
+  return (
+    <div className="center">
+      <h1>This is the home page</h1>
+    </div>
+  );
+}
