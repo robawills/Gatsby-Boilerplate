161da36d961ca0878cb4a024a7c7b7cbe7baafee
Rob Wills <robwills@Robs-MacBook-Pro.local>
2020-10-18 09:42:21 +0100

    Add strapi

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,32 @@
+// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+
+export default {
+  pathPrefix: '/bp',
+  siteMetadata: {
+    title: "Boilerplate",
+    description: "Gatsby & Strapi",
+    author: "Rob Wills",
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    
+    {
+        resolve: 'gatsby-source-strapi',
+        options: {
+            apiURL: process.env.API_URL || "http://localhost:1337",
+            contentTypes: [
+            // List of the Content Types you want to be able to request from Gatsby.
+            'article',
+            'user'
+          ],
+          queryLimit: 1000,
+        },
+    },
+    'gatsby-plugin-offline',
+  ],
+
+};
---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bf94e63ae51c358c396f31547f658c30'),
+    },
+  },
+});
