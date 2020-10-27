b2bdb95ff01ff204b5fed41ac9ee15b29c03e649
Rob Wills <robwills@Robs-MacBook-Pro.local>
2020-10-27 20:22:46 +0000

    Add get form to boilerplate

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,32 +1,75 @@
 import React, { useEffect } from 'react';
-import kwesforms from 'kwesforms';
+//import kwesforms from 'kwesforms';
+import { graphql, useStaticQuery } from 'gatsby';
+// import styled from 'styled-components';
+
+
 
 export default function HomePage({ data }) {
-  const hero = data.home;
-  useEffect (() => {
-    kwesforms.init();
-  })
+  // const hero = data.gcms.homePages;
+  const {gcms: {heroes}} = useStaticQuery(pageQuery) 
+  
+  // useEffect (() => {
+  //   kwesforms.init();
+  // })
+
+  useEffect(() => {
+    const formElement = document.querySelector('#contact-form');
+    const sentMessage = document.querySelector('.form__sent')
+
+    formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const data = new URLSearchParams(new FormData(formElement));
+      fetch('https://getform.io/f/dd73df86-1c8d-44dd-9fbe-5b91a91fbc36', {
+        method: 'post',
+        body: data
+      });
+      sentMessage.classList.add("sent");
+      document.getElementById("contact-form").reset();
+    });
+  });
 
   return (
     <div className="center">
-      <h1>{hero.Test}</h1>
+      
+      {heroes.map(({...hero}) => (
+        <h1 key="{hero.id}">{hero.heroHeadline}</h1>
+      ))}
 
-      <form class="kwes-form" action="https://kwes.io/api/foreign/forms/bMCsqtQkSmQcwqNiKN8S">
-          <label for="name">Your Name</label>
-          <input type="text" name="name" rules="required|max:255"/>
-
-          <button type="submit">Submit</button>
+      <form id="contact-form" action="https://getform.io/f/9cc66643-f264-4629-8b90-d022b29ab8d6" method="POST">
+        <input type="text" name="name"/>
+        <input type="email" name="email"/>
+        <input type="text" name="message"/>
+        <button className="form__button" type="submit">Send</button>
       </form>
+      <div 
+        className="form__sent"
+        >
+        <h3>Thank you!</h3>
+        <p>Your message has been successfully sent</p>
+      </div>
 
     </div>
   );
 }
 
-export const query = graphql`
-  
+// export const query = graphql`
+//   query {
+//     gcms {
+//       homePages {
+//         heroText
+//       }
+//     }
+//   }
+// `;
+
+export const pageQuery = graphql`
   query {
-    home: strapiHomePage {
-      Test
+    gcms {
+      heroes(first: 1) {
+        heroHeadline
+        id
+      }
     }
   }
 `;
