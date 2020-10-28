8fce6c281529b5d437833c175ed107f8a4f45652
Rob Wills <robwills@Robs-MacBook-Pro.local>
2020-10-28 20:37:06 +0000

    Add graphCms

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,15 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `graphql`,
+        fieldName: `gcms`,
+        url: `https://api-eu-central-1.graphcms.com/v2/ckgtuhwz754v801xjbprb9fpe/master`,
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -26,6 +35,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,44 @@
-import React from "react"
+import React from "react";
+import { graphql, useStaticQuery } from 'gatsby';
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export default function IndexPage({ data }) {
 
-export default IndexPage
+  const {gcms: {homePages}} = useStaticQuery(pageQuery) 
+  
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      {homePages.map(({...homePage}) =>
+        <h1 key="{homePage.id}">{homePage.heroText}</h1>
+      )}
+
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+  
+}
+
+export const pageQuery = graphql`
+  query {
+    gcms {
+      homePages(first: 1) {
+        heroText
+        id
+      }
+    }
+  }
+`;
+
+
